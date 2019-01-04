4f9eb1cbc5bca9738f8a8199270acf6ba2001024
Adam Murray <adam.murray@lifeway.com>
2019-01-04 15:56:03 -0600

    Remove feed plugin for now. Update 404 copy.

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFoundPage = () => (
   <div>
     <h1>We can&#39;t find what you&#39;re looking for</h1>
-    <p>Sorry! It just doesn&#39;t exist here.</p>
+    <p>Sorry! It&#39;s not here.</p>
   </div>
 )
 
